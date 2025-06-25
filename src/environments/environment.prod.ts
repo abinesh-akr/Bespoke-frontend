1c0eca28d10d1a36c07af12c9062a49f835d89f1
abinesh-akr <12345sabinesh_bcs26@mepcoeng.ac.in>
2025-06-25 14:58:10 +0530

    Delete src directory

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-  // src/environments/environment.prod.ts
-  export const environment = {
-    production: true,
-    apiUrl: 'https://bespoke-backend-lqt3.onrender.com' // Production URL
-  };
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-    production: false,
-    apiUrl: 'https://bespoke-backend-lqt3.onrender.com' // Development URL
-  };
-  
